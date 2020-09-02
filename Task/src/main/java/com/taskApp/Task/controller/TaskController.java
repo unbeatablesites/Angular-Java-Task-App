2a4df37b98eae18255d94d22a2380fd1a69aefff
Frank Uzoka <Frankuzoka@hotmail.com>
2020-09-01 21:47:17 -0400

    Add controller

---
+++ Task/src/main/java/com/taskApp/Task/controller/TaskController.java
@@ -0,0 +1,13 @@
+package com.taskApp.Task.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/tasks")
+public class TaskController {
+
+
+
+}
